c1235f4bd8f9c38303e545630b7d2c97e44896bf
danggg <dangvu0502@gmail.com>
2025-08-05 15:41:41 +0700

    Problem-1: Add comment

--- src/problem1/index.js
+++ src/problem1/index.js
@@ -1,11 +1,17 @@
+// Time Complexity: O(1)
+// Space Complexity: O(1)
 function sum_to_n_a(n) {
   return (n * (n + 1)) / 2;
 };
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 function sum_to_n_b(n) {
   return Array.from({ length: n }, (_, i) => i + 1).reduce((acc, curr) => acc + curr, 0);
 };
 
+// Time Complexity: O(n)
+// Space Complexity: O(n)
 function sum_to_n_c(n) {
   if (n <= 0) return 0;
   return n + sum_to_n_c(n - 1);
